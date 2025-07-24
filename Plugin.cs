ba2e1cf468e641357cf2964cacb083521f06b246
iiDk-the-actual <54154105+iiDk-the-actual@users.noreply.github.com>
2025-07-23 23:02:57 -0400

    Add more custom properties

--- Plugin.cs
+++ Plugin.cs
@@ -1,14 +1,15 @@
 ﻿using BepInEx;
-using Photon.Realtime;
-using PlayFab.ClientModels;
-using PlayFab;
-using System;
-using System.Collections.Generic;
-using UnityEngine;
-using HarmonyLib;
-using System.Text.RegularExpressions;
 using GorillaNetworking;
+using HarmonyLib;
 using Photon.Pun;
+using Photon.Realtime;
+using PlayFab;
+using PlayFab.ClientModels;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using UnityEngine;
 
 namespace TooMuchInfo
 {
@@ -52,7 +53,7 @@ namespace TooMuchInfo
                 { "BANANAOS", new string[] { "BANANAOS", "FFFF00" } }, 
                 { "GC", new string[] { "GORILLACRAFT", "43B581" } }, 
                 { "CarName", new string[] { "GORILLAVEHICLES", "43B581" } }, 
-                { "6XpyykmrCthKhFeUfkYGxv7xnXpoe2", new string[] { "CCMV2", "BF00FC" } }, 
+                { "6p72ly3j85pau2g9mda6ib8px", new string[] { "CCMV2", "BF00FC" } }, 
                 { "cronos", new string[] { "CRONOS", "0000FF" } }, 
                 { "ORBIT", new string[] { "ORBIT", "FFFFFF" } }, 
                 { "Violet On Top", new string[] { "VIOLET", "DF6BFF" } }, 
@@ -63,11 +64,44 @@ namespace TooMuchInfo
                 { "Vivid", new string[] { "VIVID", "F000BC" } }, 
                 { "RGBA", new string[] { "CUSTOMCOSMETICS", "FF0000" } },
                 { "cheese is gouda", new string[] { "WHOSICHEATING", "707070" } },
-                { "I like cheese", new string[] { "RECROOMRIG", "FE8232" } } };
+                { "shirtversion", new string[] { "GORILLASHIRTS", "707070" } },
+                { "gpronouns", new string[] { "GORILLAPRONOUNS", "707070" } },
+                { "gfaces", new string[] { "GORILLAFACES", "707070" } },
+                { "monkephone", new string[] { "MONKEPHONE", "707070" } },
+                { "pmversion", new string[] { "PLAYERMODELS", "707070" } },
+                { "gtrials", new string[] { "GORILLATRIALS", "707070" } },
+                { "msp", new string[] { "MONKESMARTPHONE", "707070" } },
+                { "gorillastats", new string[] { "GORILLASTATS", "707070" } },
+                { "using gorilladrift", new string[] { "GORILLADRIFT", "707070" } },
+                { "monkehavocversion", new string[] { "MONKEHAVOC", "707070" } },
+                { "tictactoe", new string[] { "TICTACTOE", "a89232" } },
+                { "ccolor", new string[] { "INDEX", "0febff" } },
+                { "imposter", new string[] { "GORILLAAMONGUS", "ff0000" } },
+                { "spectapeversion", new string[] { "SPECTAPE", "707070" } },
+                { "cats", new string[] { "CATS", "707070" } },
+                { "made by biotest05 :3", new string[] { "DOGS", "707070" } },
+                { "fys cool magic mod", new string[] { "FYSMAGICMOD", "707070" } },
+                { "colour", new string[] { "CUSTOMCOSMETICS", "707070" } },
+                { "chainedtogether", new string[] { "CHAINED TOGETHER", "707070" } },
+                { "goofywalkversion", new string[] { "GOOFYWALK", "707070" } },
+                { "void_menu_open", new string[] { "VOID", "303030" } },
+                { "violetpaiduser", new string[] { "VIOLETPAID", "DF6BFF" } },
+                { "violetfree", new string[] { "VIOLETFREE", "DF6BFF" } },
+                { "obsidianmc", new string[] { "OBSIDIAN.LOL", "303030" } },
+                { "hidden menu", new string[] { "HIDDEN", "707070" } },
+                { "oblivionuser", new string[] { "OBLIVION", "5055d3" } },
+                { "hgrehngio889584739_hugb\n", new string[] { "RESURGENCE", "470050" } },
+                { "eyerock reborn", new string[] { "EYEROCK", "707070" } },
+                { "asteroidlite", new string[] { "ASTEROID LITE", "707070" } },
+                { "cokecosmetics", new string[] { "COKE COSMETX", "00ff00" } } };
+
+            Dictionary<string, object> customProps = new Dictionary<string, object>();
+            foreach (DictionaryEntry dictionaryEntry in creator.GetPlayerRef().CustomProperties)
+                customProps[dictionaryEntry.Key.ToString().ToLower()] = dictionaryEntry.Value;
 
             foreach (KeyValuePair<string, string[]> specialMod in specialModsList)
             {
-                if (creator.GetPlayerRef().CustomProperties.ContainsKey(specialMod.Key))
+                if (customProps.ContainsKey(specialMod.Key.ToLower()))
                     specialMods += (specialMods == "" ? "" : ", ") + "<color=#" + specialMod.Value[1] + ">" + specialMod.Value[0] + "</color>";
             }
 
