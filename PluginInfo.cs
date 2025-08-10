603c66e4293d1a0f94eb3df3d532175166b8ca0b
ZlothY <badzacgaming@gmail.com>
2025-08-10 21:57:52 +0100

    Update PluginInfo.cs

--- PluginInfo.cs
+++ PluginInfo.cs
@@ -8,6 +8,6 @@
         public const string GUID = "zlothy.toomuchinfo";
         public const string Name = "TooMuchInfo";
         public const string HashKey = "github.com/ZlothY29IQ/TooMuchInfo";
-        public const string Version = "1.0.6";
+        public const string Version = "1.0.7";
     }
 }
